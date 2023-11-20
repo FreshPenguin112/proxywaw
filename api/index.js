78a98bc4e8fba9ae25e9613b98181bdc3fb97336
Fresh <93781766+FreshPenguin112@users.noreply.github.com>
2023-11-20 17:16:11 -0600

    proxytugfijbgv

---
+++ api/index.js
@@ -0,0 +1,25 @@
+const axios = require('axios').default;
+export default function handler(req, res, next) {
+  async function getURI(url) {
+    try {
+      const response = await axios({url});
+      if (response.status !== 200) {
+        return res.status(response.status).json({ type: 'error', message: response.statusText });
+      } else {
+      res.json(JSON.stringify(response.data))
+      }
+    } catch (error) {
+      console.log(error.message, "ERR")
+      // return res.status(500).json({ type: 'error', message: error.message });
+    }
+  
+  res.setHeader('Access-Control-Allow-Origin', req.get("origin"));
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Methods', '*')
+  res.setHeader('Access-Control-Allow-Headers', "Authorization,authorization")
+  res.setHeader('Content-Type', 'application/json');
+  if (req.method === "OPTIONS"){req.status(200).end()}else{
+    getURI(JSON.parse(req.body)['my-url'])
+}
+}
+}
